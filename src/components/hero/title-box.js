843f16745bb3d40c5b5d89ff151802db3be5c4db
V <victorvidican@gmail.com>
2022-10-07 00:25:53 +0300

    added files for github pages

---
+++ src/components/hero/title-box.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "../hero/title-box.css";
+
+export const MainTitle = () => {
+  return (
+    <div>
+      <h1 className="main-header">XLEARN</h1>
+    </div>
+  );
+};
---
+++ src/pages/hero.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { MainTitle } from "../components/hero/title-box";
+
+import symbol from "../components/imgs/symbolnew.svg";
+
+import "../pages/hero.css";
+
+const Hero = () => {
+  return (
+    <div>
+      <div className="hero-container"></div>
+
+      <div className="main-header">
+        <h1>XLEARN</h1>
+        <span className="subtitle">The new way of mastering medical sh!t</span>
+        <button className="btn">Learn More</button>
+      </div>
+      <div className="svg-container">
+        <img src={symbol} className="symbol-svg" />
+        {/* <svg>
+          <use xlinkHref={symbol} />
+        </svg> */}
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
