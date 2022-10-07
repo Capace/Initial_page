e92a7322960ce03a1ce219e90aa0db347d93f3ab
V <victorvidican@gmail.com>
2022-10-07 10:47:47 +0300

    Updates

---
+++ precache-manifest.b60e69d06fd027cd7fbe00650e5a2034.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5638ec331e9205254f3",
+    "url": "/main-page/static/css/main.24a355f2.chunk.css"
+  },
+  {
+    "revision": "f5638ec331e9205254f3",
+    "url": "/main-page/static/js/main.f5638ec3.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/main-page/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "45d599a840ff1efe516f",
+    "url": "/main-page/static/js/2.45d599a8.chunk.js"
+  },
+  {
+    "revision": "c808f15c5a8dcbd00e97",
+    "url": "/main-page/static/js/runtime~main.c808f15c.js"
+  },
+  {
+    "revision": "c167f61f6740c9d5ba9584124dddc722",
+    "url": "/main-page/static/media/symbolnew.c167f61f.svg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/main-page/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "31faf75861aa36cf1ee026fa5cb80873",
+    "url": "/main-page/static/media/human.31faf758.png"
+  },
+  {
+    "revision": "2cc4f681e10fde66547be643e7968557",
+    "url": "/main-page/index.html"
+  }
+];
